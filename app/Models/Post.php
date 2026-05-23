5050c98760fb2eb7679738337132dda5856fe98e
egorgolovin395 <olkavolosina395@gmail.com>
2026-05-23 10:57:17 +0500

    10 работа

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -12,15 +14,17 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'content', 'category_id', 'thumbnail'];
+
     public function tags()
     {
-       return $this->belongsToMany(Tag::class)->withTimestamps();
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
+
     public function sluggable(): array
     {
         return [
@@ -28,5 +32,25 @@ class Post extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    public static function uploadImage(Request $request, $image = null)
+    {
+        if ($request->hasFile('thumbnail')) {
+            if ($image) {
+                Storage::delete($image);
+            }
+            $folder = date('Y-m-d');
+            return $request->file('thumbnail')->store("images/{$folder}");
+        }
+        return null;
+    }
+
+    public function getImage()
+    {
+        if (!$this->thumbnail) {
+            return asset('no-image.png');
+        }
+        return asset("uploads/{$this->thumbnail}");
     }
 }
